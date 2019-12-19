9344b24ee82d43208d01a45b5b949b62add2328d
Reavern <f.sujanto@gmail.com>
2019-12-19 10:15:02 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * @format
+ */
+import index from './src/index';
+
+{ index }
---
+++ react-native-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    dependencies: {
+        'appcenter': {
+            platforms: {
+                android: null, // disable Android platform, other platforms will still autolink if provided
+            },
+        },
+        'appcenter-analytics': {
+            platforms: {
+                android: null, // disable Android platform, other platforms will still autolink if provided
+            },
+        },
+        'appcenter-crashes': {
+            platforms: {
+                android: null, // disable Android platform, other platforms will still autolink if provided
+            },
+        },
+        'react-native-navigation': {
+            platforms: {
+                android: null, // disable Android platform, other platforms will still autolink if provided
+            },
+        },
+    },
+};
